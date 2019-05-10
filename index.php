2dc193f15d272e6f4d472f643fd7726ef645f77d
Fabrizio55 <42611340+Fabrizio55@users.noreply.github.com>
2019-05-10 10:00:08 -0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Ejemplo</title>
+</head>
+<body>
+    <?php
+   /*echo <<<END
+    DGHFHFHFGHGH 
+END;*/
+    /*echo "Mi nombre "."es"." Fabrizio";*/
+    //Comentario 1
+    #Cometario 2
+    /*Comentario 3*/
+    $edad = 24;
+    $nombre = 'Fabrizio';
+    $altura = 1.80;
+    $mayorEdad = true;
+    echo  "Tengo ".$edad." Mi nombre es ".$nombre." Mido ".$altura." Soy ".$mayorEdad;
+    ?>
+
+</body>
+</html>
